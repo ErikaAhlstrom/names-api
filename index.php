d8bce3902321aaff6cf4c91c0ccedbdc3c0464f9
“erikaahlstrom” <info@erikasoldh.se>
2021-04-21 14:16:28 +0200

    add practise from lektion 5

--- index.php
+++ index.php
@@ -10,25 +10,45 @@ header("Referrer-Policy: no-referrer");
 
 // Steg 2 – Skapa PHP-arrayer (testdata)
 $firstNames =
-["Åsa","Mahmud","Björn","Anna","Jimmy","Anna","Maja","Marta","Gandalf","Fiona"];
+    ["Åsa", "Mahmud", "Björn", "Anna", "Jimmy", "Anna", "Maja", "Marta", "Gandalf", "Fiona"];
 $lastNames =
-["Öberg","Al Hakim","Gustavsson","Andersson", "Rickardsson","Höglund", "Thunberg", "Pettersson","Grå","Apple"];
+    ["Öberg", "Al Hakim", "Gustavsson", "Andersson", "Rickardsson", "Höglund", "Thunberg", "Pettersson", "Grå", "Apple"];
+$gender = ["male", "female", "other"];
 
-$names = array();
+$persons = array();
 
 // Steg 3 – Skapa 10 namn och spara dessa i en ny array
 for ($i = 0; $i < 10; $i++) {
-    $name = array(
-        "firstname" => $firstNames[rand(0, 9)],
-        "lastname" => $lastNames[rand(0, 9)]
+    $new_fisrt_name = $firstNames[rand(0, 9)];
+    $new_last_name = $lastNames[rand(0, 9)];
+    $newEmailName = substr(emailFormat($new_fisrt_name), 0, 2) . substr(emailFormat($new_last_name), 0, 3);
+
+    $person = array(
+        "firstname" => $new_fisrt_name,
+        "lastname" => $new_last_name,
+        "age" => rand(1, 100),
+        "gender" => $gender[rand(0, 2)],
+        "email" => strtolower($newEmailName) . "@example.com"
     );
-    array_push($names, $name);
+    array_push($persons, $person);
 }
 // Steg 4 – Konvertera PHP-arrayen till JSON
 $json = json_encode(
-    $names, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+    $persons,
+    JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT
+);
 
 // Steg 5 – Skicka JSON till klienten
 echo $json;
 
+function emailFormat($string)
+{
+    $search  = array('/Å/', '/Ä/', '/Ö/','/å/', '/ä/', '/ö/', '/-/', '/ /');
+    $replace = array('a', 'a', 'o','a', 'a', 'o', '', '');
+    $string = preg_replace($search, $replace, $string);
+    $string = strtolower($string);
+
+    return $string;
+}
 ?>
+
